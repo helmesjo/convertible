2d0030c8e3d5d2ebe31395a26632d04739505450
helmesjo <helmesjo@gmail.com>
2021-05-29 16:18:07 +0200

    Initial commit.

---
+++ tests/convertible_tests.cpp
@@ -0,0 +1,14 @@
+#include <convertible/convertible.hpp>
+
+#include <catch2/catch.hpp>
+
+SCENARIO("convertible_tests")
+{
+    GIVEN("this")
+    {
+        THEN("that")
+        {
+            REQUIRE(false);
+        }
+    }
+}
