a24ea55bf864c362f22c7e292152a8b15a822104
helmesjo <helmesjo@gmail.com>
2021-10-06 22:40:21 +0200

    Just some simple restructuring and playing with member variable pointers to see how that will fit.

--- convertible/convertible_v2.test.cxx
+++ convertible/convertible_v2.test.cxx
@@ -7,38 +7,101 @@
 
 #define FWD(...) ::std::forward<decltype(__VA_ARGS__)>(__VA_ARGS__)
 
+namespace traits
+{
+    namespace details
+    {
+        template<typename M>
+        struct member_ptr_meta
+        {
+            template<typename R>
+            struct return_t{ using type = R; };
+
+            template <typename C, typename R>
+            static return_t<C> get_class_type(R C::*);
+
+            template <typename C, typename R>
+            static return_t<R> get_value_type(R C::*);
+
+            using class_t = typename decltype(get_class_type(std::declval<M>()))::type;
+            using value_t = typename decltype(get_value_type(std::declval<M>()))::type;
+        };
+    }
+
+    template<typename member_ptr_t>
+    using member_class_t = typename details::member_ptr_meta<member_ptr_t>::class_t;
+    template<typename member_ptr_t, typename class_t = member_class_t<member_ptr_t>, typename value_t = typename details::member_ptr_meta<member_ptr_t>::value_t>
+    using member_value_t = std::conditional_t<std::is_rvalue_reference_v<class_t>, value_t&&, value_t>;
+}
+
 namespace adapters
 {
-    template<typename value_t>
-    struct value
+    template<typename obj_t>
+    struct object
     {
-        value_t& inst_;
+        obj_t& obj_;
 
-        explicit value(value_t& inst): inst_(inst){}
+        explicit object(obj_t& obj): obj_(obj){}
 
-        operator value_t() const
+        operator auto() const
         {
-            return inst_;
+            return obj_;
         }
 
-        auto operator=(const value& val)
+        auto operator=(const object& val)
+        {
+            return *this = static_cast<obj_t>(val);
+        }
+
+        auto operator=(const obj_t& val)
+        {
+            return obj_ = val;
+        }
+
+        auto operator==(const object& val) const
+        {
+            return *this == static_cast<obj_t>(val);
+        }
+
+        auto operator==(const obj_t& val) const
+        {
+            return obj_ == val;
+        }
+    };
+
+    template<typename instance_t, typename member_ptr_t>
+    struct member
+    {
+        using value_t = traits::member_value_t<member_ptr_t>;
+
+        instance_t inst_;
+        member_ptr_t ptr_;
+
+        constexpr explicit member(instance_t& inst, member_ptr_t ptr): inst_(inst), ptr_(ptr){}
+
+        operator auto() const
+        {
+            return FWD(inst_).*ptr_;
+        }
+
+        auto operator=(const member& val)
         {
             return *this = static_cast<value_t>(val);
         }
 
         auto operator=(const value_t& val)
         {
-            return inst_ = val;
+            return FWD(inst_).*ptr_ = val;
         }
 
-        auto operator==(const value& val) const
+        auto operator==(const member& val) const
         {
             return *this == static_cast<value_t>(val);
         }
 
         auto operator==(const value_t& val) const
         {
-            return inst_ == val;
+            return FWD(inst_).*ptr_ == val;
         }
     };
 }
@@ -104,8 +167,8 @@ namespace operators
 SCENARIO("playground1")
 {
     std::uint32_t val1, val2 = 0;
-    adapters::value adapter1(val1);
-    adapters::value adapter2(val2);
+    adapters::object adapter1(val1);
+    adapters::object adapter2(val2);
 
     operators::assign op1;
     operators::compare op2;
@@ -117,8 +180,8 @@ SCENARIO("playground1")
 SCENARIO("playground2")
 {
     std::uint32_t val1, val2 = 0;
-    adapters::value adapter1(val1);
-    adapters::value adapter2(val2);
+    adapters::object adapter1(val1);
+    adapters::object adapter2(val2);
 
     operators::assign op1;
     operators::compare op2;
