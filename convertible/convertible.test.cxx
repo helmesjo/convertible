12b98010bc86e6a3e5125977309939ca6c02aec5
helmesjo <helmesjo@gmail.com>
2021-11-23 18:31:23 +0100

    Started adding tests for mapping_table.

--- convertible/convertible.test.cxx
+++ convertible/convertible.test.cxx
@@ -273,32 +273,86 @@ SCENARIO("convertible: Mapping")
     }
 }
 
-SCENARIO("convertible: Conversion table")
+SCENARIO("convertible: Mapping table")
 {
     using namespace convertible;
 
-    struct type_a
+    GIVEN("mapping table between \n\n\ta.val1 <-> b.val1\n\ta.val2 <-> b.val2\n")
     {
-        int val1;
-        std::string val2;
-    } lhs;
+        struct type_a
+        {
+            int val1;
+            std::string val2;
+        } lhs;
 
-    struct type_b
-    {
-        int val1;
-        std::string val2;
-    } rhs;
+        struct type_b
+        {
+            int val1;
+            std::string val2;
+        } rhs;
 
-    mapping_table table{
-        mapping( adapters::member(&type_a::val1), adapters::member(&type_b::val1) ),
-        mapping( adapters::member(&type_a::val2), adapters::member(&type_b::val2) )
-    };
+        mapping_table table{
+            mapping( adapters::member(&type_a::val1), adapters::member(&type_b::val1) ),
+            mapping( adapters::member(&type_a::val2), adapters::member(&type_b::val2) )
+        };
 
-    lhs.val1 = 10;
-    lhs.val2 = "hello";
+        WHEN("assigning lhs to rhs")
+        {
+            lhs.val1 = 10;
+            lhs.val2 = "hello";
+            table.assign<direction::lhs_to_rhs>(lhs, rhs);
 
-    table.assign<direction::lhs_to_rhs>(lhs, rhs);
+            THEN("lhs == rhs")
+            {
+                REQUIRE(lhs.val1 == rhs.val1);
+                REQUIRE(lhs.val2 == rhs.val2);
+            }
+        }
+        WHEN("assigning lhs (r-value) to rhs")
+        {
+            lhs.val1 = 10;
+            lhs.val2 = "hello";
+            table.assign<direction::lhs_to_rhs>(std::move(lhs), rhs);
 
-    REQUIRE(lhs.val1 == rhs.val1);
-    REQUIRE(lhs.val2 == rhs.val2);
+            THEN("rhs is assigned")
+            {
+                REQUIRE(rhs.val1 == 10);
+                REQUIRE(rhs.val2 == "hello");
+
+                AND_THEN("lhs is moved from")
+                {
+                    REQUIRE(lhs.val2 == "");
+                }
+            }
+        }
+        WHEN("assigning rhs to lhs")
+        {
+            rhs.val1 = 10;
+            rhs.val2 = "hello";
+            table.assign<direction::rhs_to_lhs>(lhs, rhs);
+
+            THEN("lhs == rhs")
+            {
+                REQUIRE(lhs.val1 == rhs.val1);
+                REQUIRE(lhs.val2 == rhs.val2);
+            }
+        }
+        WHEN("assigning rhs (r-value) to lhs")
+        {
+            rhs.val1 = 10;
+            rhs.val2 = "hello";
+            table.assign<direction::rhs_to_lhs>(lhs, std::move(rhs));
+
+            THEN("lhs is assigned")
+            {
+                REQUIRE(lhs.val1 == 10);
+                REQUIRE(lhs.val2 == "hello");
+
+                AND_THEN("rhs is moved from")
+                {
+                    REQUIRE(rhs.val2 == "");
+                }
+            }
+        }
+    }
 }
