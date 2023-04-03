7e5b42a15c2825f226e1632cbd4f618de91daa64
helmesjo <helmesjo@gmail.com>
2023-04-03 23:29:03 +0200

    Move mapping into mapping.hxx

--- libconvertible/convertible/convertible.hxx
+++ libconvertible/convertible/convertible.hxx
@@ -3,6 +3,7 @@
 #include <convertible/adapter.hxx>
 #include <convertible/concepts.hxx>
 #include <convertible/common.hxx>
+#include <convertible/mapping.hxx>
 #include <convertible/operators.hxx>
 #include <convertible/readers.hxx>
 
@@ -115,72 +116,6 @@ namespace convertible
   {
     return compose(binary<first, last>(), FWD(inner)...);
   }
-
-  template<concepts::adapter _lhs_adapter_t, concepts::adapter _rhs_adapter_t, typename _converter_t = converter::identity>
-  struct mapping
-  {
-    using lhs_adapter_t = _lhs_adapter_t;
-    using rhs_adapter_t = _rhs_adapter_t;
-    using converter_t = _converter_t;
-
-    constexpr explicit mapping(lhs_adapter_t lhsAdapter, rhs_adapter_t rhsAdapter, converter_t converter = {}):
-      lhsAdapter_(std::move(lhsAdapter)),
-      rhsAdapter_(std::move(rhsAdapter)),
-      converter_(std::move(converter))
-    {}
-
-    template<direction dir>
-    constexpr void assign(concepts::adaptable<lhs_adapter_t> auto&& lhs, concepts::adaptable<rhs_adapter_t> auto&& rhs) const
-      requires requires(lhs_adapter_t lhsAdapter, rhs_adapter_t rhsAdapter, converter_t converter)
-      {
-        operators::assign{}.template operator()<dir>(lhsAdapter(FWD(lhs)), rhsAdapter(FWD(rhs)), converter);
-      }
-    {
-      operators::assign{}.template operator()<dir>(lhsAdapter_(FWD(lhs)), rhsAdapter_(FWD(rhs)), converter_);
-    }
-
-    template<direction dir = direction::rhs_to_lhs>
-    constexpr bool equal(concepts::adaptable<lhs_adapter_t> auto&& lhs, concepts::adaptable<rhs_adapter_t> auto&& rhs) const
-      requires requires(lhs_adapter_t lhsAdapter, rhs_adapter_t rhsAdapter, converter_t converter)
-      {
-        operators::equal{}.template operator()<dir>(lhsAdapter(FWD(lhs)), rhsAdapter(FWD(rhs)), converter);
-      }
-    {
-      return operators::equal{}.template operator()<dir>(lhsAdapter_(FWD(lhs)), rhsAdapter_(FWD(rhs)), converter_);
-    }
-
-    template<concepts::adaptable<rhs_adapter_t> rhs_t = typename rhs_adapter_t::adaptee_value_t>
-    constexpr auto operator()(concepts::adaptable<lhs_adapter_t> auto&& lhs) const
-      requires requires(rhs_t& rhs){ this->assign<direction::lhs_to_rhs>(FWD(lhs), rhs); }
-    {
-      rhs_t rhs = defaulted_rhs();
-      assign<direction::lhs_to_rhs>(FWD(lhs), rhs);
-      return rhs;
-    }
-
-    template<concepts::adaptable<lhs_adapter_t> lhs_t = typename lhs_adapter_t::adaptee_value_t>
-    constexpr auto operator()(concepts::adaptable<rhs_adapter_t> auto&& rhs) const
-      requires requires(lhs_t& lhs){ this->assign<direction::rhs_to_lhs>(lhs, FWD(rhs)); }
-    {
-      lhs_t lhs = defaulted_lhs();
-      assign<direction::rhs_to_lhs>(lhs, FWD(rhs));
-      return lhs;
-    }
-
-    constexpr auto defaulted_lhs() const
-    {
-      return lhsAdapter_.defaulted_adaptee();
-    }
-
-    constexpr auto defaulted_rhs() const
-    {
-      return rhsAdapter_.defaulted_adaptee();
-    }
-
-    lhs_adapter_t lhsAdapter_;
-    rhs_adapter_t rhsAdapter_;
-    converter_t converter_;
-  };
 
   template<typename callback_t, typename tuple_t>
   constexpr bool for_each(callback_t&& callback, tuple_t&& pack)
---
+++ libconvertible/convertible/mapping.hxx
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <convertible/concepts.hxx>
+#include <convertible/operators.hxx>
+#include <convertible/readers.hxx>
+
+#define FWD(...) ::std::forward<decltype(__VA_ARGS__)>(__VA_ARGS__)
+
+namespace convertible
+{
+template<concepts::adapter _lhs_adapter_t, concepts::adapter _rhs_adapter_t, typename _converter_t = converter::identity>
+  struct mapping
+  {
+    using lhs_adapter_t = _lhs_adapter_t;
+    using rhs_adapter_t = _rhs_adapter_t;
+    using converter_t = _converter_t;
+
+    constexpr explicit mapping(lhs_adapter_t lhsAdapter, rhs_adapter_t rhsAdapter, converter_t converter = {}):
+      lhsAdapter_(std::move(lhsAdapter)),
+      rhsAdapter_(std::move(rhsAdapter)),
+      converter_(std::move(converter))
+    {}
+
+    template<direction dir>
+    constexpr void assign(concepts::adaptable<lhs_adapter_t> auto&& lhs, concepts::adaptable<rhs_adapter_t> auto&& rhs) const
+      requires requires(lhs_adapter_t lhsAdapter, rhs_adapter_t rhsAdapter, converter_t converter)
+      {
+        operators::assign{}.template operator()<dir>(lhsAdapter(FWD(lhs)), rhsAdapter(FWD(rhs)), converter);
+      }
+    {
+      operators::assign{}.template operator()<dir>(lhsAdapter_(FWD(lhs)), rhsAdapter_(FWD(rhs)), converter_);
+    }
+
+    template<direction dir = direction::rhs_to_lhs>
+    constexpr bool equal(concepts::adaptable<lhs_adapter_t> auto&& lhs, concepts::adaptable<rhs_adapter_t> auto&& rhs) const
+      requires requires(lhs_adapter_t lhsAdapter, rhs_adapter_t rhsAdapter, converter_t converter)
+      {
+        operators::equal{}.template operator()<dir>(lhsAdapter(FWD(lhs)), rhsAdapter(FWD(rhs)), converter);
+      }
+    {
+      return operators::equal{}.template operator()<dir>(lhsAdapter_(FWD(lhs)), rhsAdapter_(FWD(rhs)), converter_);
+    }
+
+    template<concepts::adaptable<rhs_adapter_t> rhs_t = typename rhs_adapter_t::adaptee_value_t>
+    constexpr auto operator()(concepts::adaptable<lhs_adapter_t> auto&& lhs) const
+      requires requires(rhs_t& rhs){ this->assign<direction::lhs_to_rhs>(FWD(lhs), rhs); }
+    {
+      rhs_t rhs = defaulted_rhs();
+      assign<direction::lhs_to_rhs>(FWD(lhs), rhs);
+      return rhs;
+    }
+
+    template<concepts::adaptable<lhs_adapter_t> lhs_t = typename lhs_adapter_t::adaptee_value_t>
+    constexpr auto operator()(concepts::adaptable<rhs_adapter_t> auto&& rhs) const
+      requires requires(lhs_t& lhs){ this->assign<direction::rhs_to_lhs>(lhs, FWD(rhs)); }
+    {
+      lhs_t lhs = defaulted_lhs();
+      assign<direction::rhs_to_lhs>(lhs, FWD(rhs));
+      return lhs;
+    }
+
+    constexpr auto defaulted_lhs() const
+    {
+      return lhsAdapter_.defaulted_adaptee();
+    }
+
+    constexpr auto defaulted_rhs() const
+    {
+      return rhsAdapter_.defaulted_adaptee();
+    }
+
+    lhs_adapter_t lhsAdapter_;
+    rhs_adapter_t rhsAdapter_;
+    converter_t converter_;
+  };
+}
+
+#undef FWD
